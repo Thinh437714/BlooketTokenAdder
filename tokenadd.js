ab1cc4eab45a3b652c0d76614f0f862d2cd5a8d6
Thinh437714 <99885438+Thinh437714@users.noreply.github.com>
2022-02-18 09:32:51 +0700

    Update tokenadd.js

--- tokenadd.js
+++ tokenadd.js
@@ -1,8 +1,8 @@
-accname = prompt('Enter your blooket username');tokensamount = prompt('How many tokens do you want (max 500 daily)');fetch("https://api.blooket.com/api/users/addtokens", {
+accname = ('Enter your blooket username'); = ("https://api.blooket.com/api/users/addtokens", {
   "headers": {
     "accept": "application/json, text/plain, */*",
     "accept-language": "en-US,en;q=0.9",
-    "authorization": localStorage.token,
+    "authorization": .token,
     "content-type": "application/json;charset=UTF-8",
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
